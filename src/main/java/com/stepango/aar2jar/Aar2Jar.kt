98a35ca1fc2d8da3f88a11cfa879bc15ffb1aa47
Stepan Goncharov <step.89.g@gmail.com>
2019-05-21 18:50:18 +0300

    Make plugin dynamic

--- src/main/java/com/stepango/aar2jar/Aar2Jar.kt
+++ src/main/java/com/stepango/aar2jar/Aar2Jar.kt
@@ -7,7 +7,6 @@ import org.gradle.api.artifacts.type.ArtifactTypeDefinition
 import org.gradle.api.internal.artifacts.ArtifactAttributes.ARTIFACT_FORMAT
 import org.gradle.api.plugins.JavaPluginConvention
 import org.gradle.kotlin.dsl.dependencies
-import org.gradle.kotlin.dsl.get
 import org.gradle.kotlin.dsl.the
 import java.io.File
 import java.io.FileOutputStream
@@ -16,40 +15,48 @@ import java.util.zip.ZipFile
 class Aar2Jar : Plugin<Project> {
 
     override fun apply(project: Project) {
-        project.configurations
-                .register("compileOnlyAar")
-                .configure {
-                    isTransitive = false
-                    attributes {
-                        attribute(ARTIFACT_FORMAT, ArtifactTypeDefinition.JAR_TYPE)
-                    }
-
-                    project.the<JavaPluginConvention>()
-                            .sourceSets["main"]
-                            .compileClasspath += this@configure
-                }
-
-        project.configurations
-                .register("implementationAar")
-                .configure {
-                    isTransitive = false
-                    attributes {
-                        attribute(ARTIFACT_FORMAT, ArtifactTypeDefinition.JAR_TYPE)
-                    }
-
-                    project.the<JavaPluginConvention>()
-                            .sourceSets["main"]
-                            .apply {
-                                compileClasspath += this@configure
-                                runtimeClasspath += this@configure
-                            }
-                }
 
         project.dependencies {
             registerTransform {
                 from.attribute(ARTIFACT_FORMAT, "aar")
                 to.attribute(ARTIFACT_FORMAT, ArtifactTypeDefinition.JAR_TYPE)
                 artifactTransform(AarToJarTransform::class.java)
+            }
+        }
+
+        project.plugins.whenPluginAdded {
+            if (this.javaClass.name == "org.gradle.api.plugins.JavaBasePlugin") {
+                project.configurations
+                        .register("compileOnlyAar")
+                        .configure {
+                            isTransitive = false
+                            attributes {
+                                attribute(ARTIFACT_FORMAT, ArtifactTypeDefinition.JAR_TYPE)
+                            }
+
+                            project.the<JavaPluginConvention>()
+                                    .sourceSets.whenObjectAdded {
+                                if (name == "main") {
+                                    compileClasspath += this@configure
+                                }
+                            }
+                        }
+
+                project.configurations
+                        .register("implementationAar")
+                        .configure {
+                            isTransitive = false
+                            attributes {
+                                attribute(ARTIFACT_FORMAT, ArtifactTypeDefinition.JAR_TYPE)
+                            }
+                            project.the<JavaPluginConvention>()
+                                    .sourceSets.whenObjectAdded {
+                                if (name == "main") {
+                                    compileClasspath += this@configure
+                                    runtimeClasspath += this@configure
+                                }
+                            }
+                        }
             }
         }
     }
